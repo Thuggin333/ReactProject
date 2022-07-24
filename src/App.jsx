c147ddd4ec63b22bb2848ff7bafc1ed94c0f4137
Saqo Harutyunyan <saqo.harutyunyan.ait@gmail.com>
2022-07-24 15:53:45 +0300

    React Project 50%

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import logo from "./logo.svg";
+import "./css/style.css";
+import React from "react";
+import "./App.css";
+
+// Menue Elements
+const menuElements = ["FEATURES", "SOLUTIONS", "PLANS", "PRICING", "RESORCES"];
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header id="header">
+        <div className="logoBlockCont">
+          <div className="logoBlock">
+            <h1>TRELLO</h1>
+            <div className="menuEl">
+              {menuElements.map((el, i) => {
+                return (
+                  <a className="menuElChild" key={i} href="">
+                    {el}
+                  </a>
+                );
+              })}
+            </div>
+          </div>
+
+          <div className="register">
+            <p>Log in</p>
+            <p>Get Trello for free</p>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+// Home Section
+export class HomeSection extends React.Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="homeLeftBlock">
+          <div className="homeInfo">
+            <h2>Trello helps teams move work forward.</h2>
+            <p>
+              Collaborate, manage projects, and reach new productivity peaks.
+              From high rises to the home office, the way your team works is
+              unique—accomplish it all with Trello
+            </p>
+            <div className="input">
+              <input type="email" placeholder="Email" />
+              <button>Sign-Up</button>
+            </div>
+          </div>
+        </div>
+        <div className="homeRightBlock">
+          <img
+            src="https://images.ctfassets.net/rz1oowkt5gyp/5QIzYxue6b7raOnVFtMyQs/113acb8633ee8f0c9cb305d3a228823c/hero.png?w=1200&fm=webp"
+            alt=""
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+// Solutions
+
+export class Solution extends React.Component {
+  render() {
+    return (
+      <div className="solution">
+        <div className="solutionUpBlock">
+          <div className="solutionTitle">
+            <h1 className="solutionH1">
+              It’s more than work. It’s a way of working together.
+            </h1>
+            <p className="solutionP1">
+              Start with a Trello board, lists, and cards. Customize and expand
+              with more features as your teamwork grows. Manage projects,
+              organize tasks, and build team spirit—all in one place.
+            </p>
+            <button className="buttonSolution">Start Doing</button>
+          </div>
+        </div>
+        <div className="solutionDownBlock">
+          <img
+            src="https://images.ctfassets.net/rz1oowkt5gyp/7pYWhpQ3vnntxoShaImNws/777fabbf069416489167ab92027ce086/board.png?w=1200&fm=webp"
+            alt=""
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Header from "./App";
+import { HomeSection } from "./App";
+import { Solution } from "./App";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Header />
+    <HomeSection />
+    <Solution />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
